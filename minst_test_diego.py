c7726d17ac99f87817e04f74fd2a74dc02a4f2a3
Muhammad Zulqarnain <zulqarnain@hotmail.com>
2022-12-19 17:31:04 -0500

    eiggap new fig

--- minst_test_diego.py
+++ minst_test_diego.py
@@ -1,10 +1,16 @@
-#%% This code uses MINST Dataset and see the best T_c for
-#%% Import Libraries etc
+# %% This code uses MINST Dataset and see the best T_c for
+# %% Use Multi_Processing libraries
+# %% Import Libraries etc
 import numpy as np
 from matplotlib import pyplot as plt
 from functions import *
-#%% Begin define DIEGO Algorithm
-def DIEGO_MINST(W,N,d,vti,tot_iter,x_samples,pca_vect,step_size):
+import multiprocessing as mp
+import time
+import os
+
+
+# %% Begin define DIEGO Algorithm
+def DIEGO_MINST(W, N, d, vti, tot_iter, x_samples, pca_vect, step_size):
     # Data samples distribute among N nodes
     x_samples_n = np.reshape(x_samples, (N, tot_iter, d))
     # Now we initialize all N nodes with initial eigenvector vti
@@ -22,19 +28,21 @@ def DIEGO_MINST(W,N,d,vti,tot_iter,x_samples,pca_vect,step_size):
             upd_n[:, i_n] = (x_n @ x_n.T @ vn)
         upd_n = upd_n @ W
         # Update eigenvector estimate
-        v_n = v_n + gamma_ext * N*upd_n # for fully connected graph. scale by 1/[W^*e_1]i i.e scale by N
+        v_n = v_n + gamma_ext * N * upd_n  # for fully connected graph. scale by 1/[W^*e_1]i i.e scale by N
         # Normalize the estimate to make unit norm
         for i_n in range(0, N):
             v_n[:, i_n] = v_n[:, i_n] / np.linalg.norm(v_n[:, i_n], 2)
         # Compute Error for each iteration
         for i_n in range(0, N):
             err_n[i_n, sample] = np.sqrt(1 - ((v_n[:, i_n].T @ pca_vect) ** 2) / (
-                        np.linalg.norm(v_n[:, i_n], 2) ** 2))
+                    np.linalg.norm(v_n[:, i_n], 2) ** 2))
     # Compute mean error across all nodes at each iteration. For fully connected this is error across any node.
     mean_err = np.squeeze(np.array(np.mean(err_n, axis=0)))
     return mean_err
-#%% Begin define CDIEGO Algorithm
-def CDIEGO_MINST(W,N,d,vti,tot_iter,x_samples,pca_vect,step_size,Tc):
+
+
+# %% Begin define CDIEGO Algorithm
+def CDIEGO_MINST(W, N, d, vti, tot_iter, x_samples, pca_vect, step_size, Tc):
     # Data samples distribute among N nodes
     x_samples_n = np.reshape(x_samples, (N, tot_iter, d))
     # Now we initialize all N nodes with initial eigenvector vti
@@ -50,7 +58,7 @@ def CDIEGO_MINST(W,N,d,vti,tot_iter,x_samples,pca_vect,step_size,Tc):
             x_n = Column(x_samples_n[i_n, sample, :])  # Draw sample across node i_n
             vn = v_n[:, i_n]  # Draw current estimate of eigenvector across node i_n
             upd_n[:, i_n] = (x_n @ x_n.T @ vn)
-        for round in range(0,Tc): # Run for multiple communication rounds.
+        for round in range(0, Tc):  # Run for multiple communication rounds.
             upd_n = upd_n @ W
         # After updating upd_n data. The next thing is to scale it by W^R instead of N.
         W_e1 = (W ** Tc)
@@ -59,7 +67,8 @@ def CDIEGO_MINST(W,N,d,vti,tot_iter,x_samples,pca_vect,step_size,Tc):
             print('Failed to run. Weight matrix has a zero. Try increasing Tmix')
             break
         else:
-            W_e1 = W_e1[:, 0] # + 1e-100 # as we are taking first column only. We need to add small eps to avoid scaling by 1/0
+            W_e1 = W_e1[:,
+                   0]  # + 1e-100 # as we are taking first column only. We need to add small eps to avoid scaling by 1/0
         # Update eigenvector estimate
         for i_n in range(0, N):
             v_n[:, i_n] = v_n[:, i_n] + gamma_ext * (upd_n[:, i_n]) / (W_e1[i_n]).item()
@@ -69,63 +78,145 @@ def CDIEGO_MINST(W,N,d,vti,tot_iter,x_samples,pca_vect,step_size,Tc):
         # Compute Error for each iteration
         for i_n in range(0, N):
             err_n[i_n, sample] = np.sqrt(1 - ((v_n[:, i_n].T @ pca_vect) ** 2) / (
-                        np.linalg.norm(v_n[:, i_n], 2) ** 2))
+                    np.linalg.norm(v_n[:, i_n], 2) ** 2))
     max_err = np.squeeze(np.array(np.max(err_n, axis=0)))
-    return max_err # Give out max error among all nodes from q_1 instead of mean error.
+    return max_err  # Give out max error among all nodes from q_1 instead of mean error.
 
-#%% Begin Main Implementation
+
+# %% Begin Main Implementation
 ## Load MINST DATASET
 import pickle
 import gzip
-# from keras.datasets import mnist
-# (train_X, train_y), (test_X, test_y) = mnist.load_data()
+
 # Load the dataset
 (train_inputs, train_targets), (valid_inputs, valid_targets), (test_inputs, test_targets) = pickle.load(
     gzip.open('MINST_dataset/mnist_py3k.pkl.gz', 'rb'))
-train_inputs = np.concatenate((train_inputs, valid_inputs))
-data = train_inputs.transpose()  # dimensionxnum_samples
-#%% Concatenate data to create more samples and perform a random shuffle
-np.random.seed(1)
-data_concatenated = np.zeros((data.shape[0],data.shape[1]*20))
-for i in range(0,20):
-    data_concatenated[:,i*data.shape[1]:(i+1)*data.shape[1]] = data
-## Perform Random shuffle
-np.random.shuffle(data_concatenated)
-#%% Load True eigenvectors for MINST
-with open("MINST_dataset/EV_mnist.pickle", 'rb') as f:
-    X1 = pickle.load(f)
-pca_vect = X1[:, 0]
-#%% Load Additional Parameters
+data_concatenated = np.concatenate((train_inputs, valid_inputs))
+# %% Compute True eigenvectors for MINST (Using Batch method)
+A = (1 / 60000) * np.matrix(
+    data_concatenated.T @ data_concatenated)  # Find covariance matrix Sigma using \Sigma = \tilde{A}^T \tilde{A}
+eigv = np.linalg.eigh(A)  # Find eigenvalue decomposition of eigv
+ev = np.matrix(eigv[-1])  ## Fetch eigen vectors
+pca_vect = ev[:, -1]
+
+# %% Load Additional Parameters
 N = 10
-monte_carlo = 10
-p = 0.1 # Parameter for Erdos-Reyni Convergence
-step_size = 10
-tot_data_samples = data_concatenated.shape[1]
-d = data_concatenated.shape[0]
-tot_iter = int(tot_data_samples/N)
-#%% Create empty array
-diego_f_cnnc_m = np.zeros((monte_carlo,tot_iter))
-# Each a b c arrays created below will save different values of Tc
-cdiego_f_cnnc_m_a = np.zeros((monte_carlo,tot_iter))
+monte_carlo = 30
+p = 0.1  # Parameter for Erdos-Reyni Convergence
+step_size = 0.01
+tot_data_samples = 60000
+d = 784
+tot_iter = int(tot_data_samples / N)
+# %% Create empty array
+cases = 3  # 3 cases to compare: diego vs cdiego (log(Nt) vs cdiego (Tmix 3/2 log(Nt)
+diego_cdiego_m = np.zeros((cases, monte_carlo, tot_iter))
+## Each a b c arrays created below will save different values of Tc
+# cdiego_f_cnnc_m_a = np.zeros((monte_carlo,tot_iter))
 # %% Generate random initial vector to be same for all monte-carlo simulations
 vti = np.random.normal(0, 1, (d))  # Generate random eigenvector
 vti = Column(vti / np.linalg.norm(vti))  # unit normalize for selection over unit sphere
-#%% Generate Fully Connected Graph for Hyp A (which assumes Federated Learning)
+# %% Generate Fully Connected Graph for Hyp A (which assumes Federated Learning)
 W_f = np.matrix(1 / N * (np.ones((N, 1)) @ np.ones((N, 1)).T))
-for mon in range(0,monte_carlo):
-    print('Currently Processing Nodes: ', N, ' of Monte Carlo: ',mon,'\n')
+# %% Generate Erdos-Renyi Graph and weight matrix using Metropolis-Hastling Algorithm
+A = gen_graph_adjacency(N, p)
+W_nf = W_gen_M(N, A)
+# %% Compute Tmix and Maximum No. Of Rounds
+Tmix = Tmix_calc(W_nf, N)
+R_a = int(np.ceil((np.log(N * (tot_iter)))))
+R_max = int(np.ceil(Tmix * (3 / 2) * (np.log(N * tot_iter))))
+# %% Store samples in all monte-carlo sim
+x_samples = np.zeros((monte_carlo, N * tot_iter, d))
+for sam in range(0, monte_carlo):
+    np.random.seed(1)
     np.random.shuffle(data_concatenated)
-    #%% Run DIEGO Algorithm
-    diego_f_cnnc_m[mon,:] = DIEGO_MINST(W_f,N,d,vti,tot_iter,data_concatenated,pca_vect,step_size)
-#%% Save The data
-np.save('sim_data/MINST_DIEGO_iter_count_' + str(tot_iter) + '_dimdata_' + str(d) + '_nodes_' + str(
-        N) + '_ss_' + str(step_size) + '_mc_' + str(monte_carlo) + '_FC_'+'.npy', diego_f_cnnc_m)
-#%% Compute Mean and Max of errors
-diego_f_cnnc = np.squeeze(np.array(np.mean(diego_f_cnnc_m, axis=0)))
-#%% Plot the results
-plt.figure()
-plt.semilogy(diego_f_cnnc, label='DIEGO, StepSize='+str(step_size)+', N= '+str(N),linestyle='dashed',linewidth=2)
-plt.title('DIEGO vs CDIEOG with diff stepsize with d= '+str(d))
-plt.ylabel('Error')
-plt.xlabel('No. of Iterations')
-plt.show()
+    x_samples[sam, :, :] = data_concatenated
+
+
+# %% Begin Parallelization for monte carlo simulations
+
+## Computes DIEGO run only
+def monte_carlo_mp_DIEGO(r):
+    total_count = r[-1] - r[0] + 1  # Count total number entries in range
+    diego_mc_p = np.zeros(
+        (total_count, tot_iter))  # Generate empty matrix to store the values of data for current thread
+    for im in range(0, total_count):
+        print('Current Monte Carlo index for DIEGO is: ', r[im], ' and index is: ', im, '\n')
+        diego_mc_p[im, :] = DIEGO_MINST(W_f, N, d, vti, tot_iter, x_samples[r[im], :, :], pca_vect,
+                                        step_size)  # Note: r[im] will be unique monte-carlo index
+    return diego_mc_p
+
+
+## Uses log(Nt) rounds
+def monte_carlo_mp_CDIEGO_Ra(r):
+    total_count = r[-1] - r[0] + 1  # Count total number entries in range
+    cdiego_Ra_mc_p = np.zeros(
+        (total_count, tot_iter))  # Generate empty matrix to store the values of data for current thread
+    for im in range(0, total_count):
+        print('Current Monte Carlo index for CDIEGO R_a is: ', r[im], ' and index is: ', im, '\n')
+        cdiego_Ra_mc_p[im, :] = CDIEGO_MINST(W_nf, N, d, vti, tot_iter, x_samples[r[im], :, :], pca_vect, step_size,
+                                             R_a)
+    return cdiego_Ra_mc_p
+
+
+## Uses T_mix * 3/2* log(Nt) rounds
+def monte_carlo_mp_CDIEGO_Rmax(r):
+    total_count = r[-1] - r[0] + 1  # Count total number entries in range
+    cdiego_R_max_mc_p = np.zeros(
+        (total_count, tot_iter))  # Generate empty matrix to store the values of data for current thread
+    for im in range(0, total_count):
+        print('Current Monte Carlo index for CDIEGO R_max is: ', r[im], ' and index is: ', im, '\n')
+        cdiego_R_max_mc_p[im, :] = CDIEGO_MINST(W_nf, N, d, vti, tot_iter, x_samples[r[im], :, :], pca_vect, step_size,
+                                                R_max)
+    return cdiego_R_max_mc_p
+
+
+## Run Main file
+start_time = time.time()
+if __name__ == '__main__':
+    ranges = [
+        # range(0, 5),
+        # range(5, 10),
+        # range(10, 15),
+        # range(15, 20),
+        # range(20, 25),
+        # range(25, 30),
+        # range(30, 35),
+        # range(35, 40),
+        # range(40, 45),
+        # range(45, 50)
+
+        # ## 10 Monte Carlo
+        # range(0, 1),
+        # range(1, 2)
+        # # range(2, 3),
+        # # range(3, 4),
+        # # range(4, 5),
+        # # range(5, 6),
+        # # range(6, 7),
+        # # range(7, 8),
+        # # range(8, 9),
+        # # range(9, 10)
+
+        ## 30 Monte Carlo
+        range(0, 15),
+        range(15, 30)
+    ]
+    # Create a threadpool with N threads
+    ## This code just parallelize each function at a time. So after DIEGO it executes CDIEGO and so on.
+    ## Simultaneously parallelization of all three cases is not yet achieved.
+    print('Starting MP process')
+    pool = mp.Pool(2)
+    result_DIEGO = pool.imap(monte_carlo_mp_DIEGO, ranges)
+    result_CDIEGO_Ra = pool.imap(monte_carlo_mp_CDIEGO_Ra, ranges)
+    result_CDIEGO_Rmax = pool.imap(monte_carlo_mp_CDIEGO_Rmax, ranges)
+    pool.close()
+    pool.join()
+    # Join the values from result run across multi workers
+    diego_cdiego_m[0, :, :] = np.concatenate(result_DIEGO, axis=0)
+    diego_cdiego_m[1, :, :] = np.concatenate(result_CDIEGO_Ra, axis=0)
+    diego_cdiego_m[2, :, :] = np.concatenate(result_CDIEGO_Rmax, axis=0)
+    filename_data = 'sim_data/MP_DIEGO_CDIEGO_MNIST_amarel_iter_count_' + str(tot_iter) + '_dimdata_' + str(
+        d) + '_nodes_' + str(
+        N) + '_ss_' + str(step_size) + '_mc_' + str(monte_carlo) + '.npy'
+    # np.save(filename_data, diego_cdiego_m)
+print("--- %s seconds ---" % (time.time() - start_time))
